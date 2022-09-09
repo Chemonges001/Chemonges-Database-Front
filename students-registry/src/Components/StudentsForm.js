ab2ff2afb07a167f1f95ad1b2af950db3e6b5e9c
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-09-09 10:54:42 +0300

    coplete front end

--- students-registry/src/Components/StudentsForm.js
+++ students-registry/src/Components/StudentsForm.js
@@ -18,38 +18,38 @@ function StudentForm({postStudent, schools}){
         postStudent(formData)
     } 
     return(
-        <div className='student_form'>
-            <header>
-            <h1 className='title'>Student Database</h1>
-            </header>
+     <div className='student_form'>
+        <header>
+        <h1 className='title'>Student Database</h1>
+        </header>
             
-//             <form onSubmit={handleSubmit}>
-//                 <label>
-//                 Name:
-//                 <input type="text" name="student_name" value={formData.student_name} onChange={handleChange}/>
-//                 </label>
-//                 <label>
-//                 Age:
-//                 <input type="number" name="age" value={formData.age} onChange={handleChange}/>
-//                 </label>
-//                 <label>
-//                 Gender:
-//                 <input type="text" name="gender" value={formData.gender} onChange={handleChange}/>
-//                 </label>
-//                 <label>
-//                 Phone number:
-//                 <input type="text" name="phone" value={formData.phone} onChange={handleChange}/>
-//                 </label>
-//                 <label>
-//                 School:
-//                 <select name="school_id" onChange={handleChange}>
-//                     <option>Select A School</option>
-//                     {schools.map(s => <option key ={s.id} value={s.id}>{s.school_name}</option>)}
-//                 </select>
-//                 </label>
-//                 <input className='submit_btn' type="submit" value="Submit" />
-//             </form>
-//         </div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input type="text" name="student_name" value={formData.student_name} onChange={handleChange}/>
+          </label>
+          <label>
+            Age:
+            <input type="number" name="age" value={formData.age} onChange={handleChange}/>
+          </label>
+          <label>
+             Gender:
+            <input type="text" name="gender" value={formData.gender} onChange={handleChange}/>
+          </label>
+          <label>
+            Phone number:
+            <input type="text" name="phone" value={formData.phone} onChange={handleChange}/>
+          </label>
+          <label>
+            School:
+           <select name="school_id" onChange={handleChange}>
+            <option>Select A School</option>
+                {schools.map(s => <option key ={s.id} value={s.id}>{s.school_name}</option>)}
+            </select>
+          </label>
+            <input className='submit_btn' type="submit" value="Submit" />
+        </form>
+        </div>
         
     )
 }
