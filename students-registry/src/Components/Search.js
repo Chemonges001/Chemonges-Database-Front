b084070a0ae3a7dc6172e28950ff0c65441ae11c
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-09-09 10:16:09 +0300

    create search function

--- students-registry/src/Components/Search.js
+++ students-registry/src/Components/Search.js
@@ -1,19 +1,19 @@
-// import React from 'react'
+import React from 'react'
 
-// function Search({ search, onSearchChange }) {
-//   return (
-//     <nav>
-//       <input
+function Search({ search, onSearchChange }) {
+  return (
+    <nav>
+      <input
     
-//         type="text"
-//         name="search"
-//         placeholder="Search..."
-//         autoComplete="off"
-//         value={search}
-//         onChange={e => onSearchChange(e.target.value)}
-//       />
-//     <button type="submit">Search</button>
-//     </nav>
-//   );
-// }
-// export default Search
+        type="text"
+        name="search"
+        placeholder="Search..."
+        autoComplete="off"
+        value={search}
+        onChange={e => onSearchChange(e.target.value)}
+      />
+    <button type="submit">Search</button>
+    </nav>
+  );
+}
+export default Search
